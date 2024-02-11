4642c1ba02e25090f5264d71f0abbd47b7bd244a
maxefimov <efimov.top.academy@gmail.com>
2024-02-11 13:17:59 +0300

    Добавьте файлы проекта.

---
+++ NetClassesStructsSpacenamesApp/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetClassesStructsSpacenamesApp
+{
+    class Employee
+    {
+        public string name;
+        public int age;
+
+        public Employee() : this("Anonim", 1) {}
+
+        public Employee(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {this.name}, Age: {this.age}");
+        }
+    }
+
+    struct BirthDate
+    {
+        public int day = 20;
+        public int month;
+        public int year;
+
+        public BirthDate() { }
+
+        public void Show()
+        {
+            Console.WriteLine($"Date: {day}.{month}.{year}");
+        }
+    }
+}
---
+++ NetClassesStructsSpacenamesApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using NetClassesStructsSpacenamesApp;
+
+Employee employee = new Employee();
+employee.name = "Bobby";
+employee.age = 25;
+Employee bob = employee;
+
+employee.Show();
+
+bob.name = "Noname";
+employee.Show();
+
+
+
+Employee joe = new Employee() { name = "Joe", age = 31 };
+
+var leo = new Employee() { name = "Leo", age = 28 };
+Employee tom = new() { name = "Tom", age = 19 };
+
+Employee Jim = new("Jim", 22);
+
+BirthDate date = new BirthDate() 
+                    { 
+                        day = 5, 
+                        month = 9, 
+                        year = 2019 
+                    };
+date.Show();
+
+BirthDate date2 = date with { day = 30 };
+//date2.day = 30;
+
+date.Show();
+date2.Show();
